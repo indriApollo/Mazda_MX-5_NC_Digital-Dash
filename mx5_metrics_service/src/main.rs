38c79232e7f84f0a3d986a6666fd5d4f66d50706
indriApollo <indri.apollo@gmail.com>
2024-10-27 21:41:28 +0100

    feat: setup epoll

--- mx5_metrics_service/src/main.rs
+++ mx5_metrics_service/src/main.rs
@@ -1,31 +1,33 @@
 use nix::sys::signal::{self, sigprocmask, Signal};
 use nix::sys::signalfd::{SigSet, SignalFd};
+use nix::sys::epoll::{Epoll, EpollCreateFlags, EpollEvent, EpollFlags, EpollTimeout};
 
 fn main() {
+    enum EpollEventId {
+        Signal
+    }
+
     let sfd = setup_signal_handler();
+
+    let epoll = Epoll::new(EpollCreateFlags::empty())
+        .expect("epoll");
+
+    epoll.add(&sfd, EpollEvent::new(EpollFlags::EPOLLIN, EpollEventId::Signal as u64))
+        .expect("epoll add");
+
     println!("Ready");
 
+    let mut events = [EpollEvent::empty()];
+
     loop {
-        match sfd.read_signal() {
-            Ok(Some(signal)) => {
-                if signal.ssi_signo == Signal::SIGINT as u32 {
-                    println!("Got SIGINT")
-                }
-                else if signal.ssi_signo == Signal::SIGTERM as u32 {
-                    println!("Got SIGTERM")
-                }
-                else {
-                    panic!("Unexpected signal: {}", signal.ssi_signo);
-                }
-                break;
-            }
-            Ok(None) => {
-                unreachable!("SIG_BLOCK")
-            },
-            Err(e) => {
-                panic!("Error reading signal: {}", e);
-            }
+        epoll.wait(&mut events, EpollTimeout::NONE)
+            .expect("epoll wait");
+
+        if events[0].data() == EpollEventId::Signal as u64 {
+            handle_signal(&sfd);
+            break;
         }
+
     }
 }
 
@@ -37,4 +39,26 @@ fn setup_signal_handler() -> SignalFd {
         .expect("sigprocmask");
 
     SignalFd::new(&sigset).expect("signalFd")
+}
+
+fn handle_signal(sfd: &SignalFd) {
+    match sfd.read_signal() {
+        Ok(Some(signal)) => {
+            if signal.ssi_signo == Signal::SIGINT as u32 {
+                println!("Got SIGINT")
+            }
+            else if signal.ssi_signo == Signal::SIGTERM as u32 {
+                println!("Got SIGTERM")
+            }
+            else {
+                panic!("Unexpected signal: {}", signal.ssi_signo);
+            }
+        }
+        Ok(None) => {
+            unreachable!("SIG_BLOCK")
+        },
+        Err(e) => {
+            panic!("Error reading signal: {}", e);
+        }
+    }
 }
